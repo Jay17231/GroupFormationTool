b833d46e1f2be72a6aae597a34df552a70be7342
Aman Vishnani <aman.vishnani@dal.ca>
2020-05-23 18:12:02 -0300

    Changed REST to MVC model.

--- src/main/java/csci5408/catme/controller/ApplicationController.java
+++ src/main/java/csci5408/catme/controller/ApplicationController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ApplicationController {
 
     @GetMapping
     public String greetings() {
-        return "Hello World";
+        return "index";
     }
 }
