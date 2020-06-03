1a8b8724380bc365de90262f4ecf3d1247fdd587
Krupa Patel <kr635164@dal.ca>
2020-06-03 02:08:24 -0300

    Database timezone error solved

--- src/main/java/csci5408/catme/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/csci5408/catme/service/impl/AuthenticationServiceImpl.java
@@ -1,12 +1,10 @@
 package csci5408.catme.service.impl;
 
-import csci5408.catme.authentication.ISessionStore;
-import csci5408.catme.dao.UserDao;
-import csci5408.catme.domain.User;
-import csci5408.catme.dto.UserSummary;
-import csci5408.catme.service.AuthenticationService;
-import csci5408.catme.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import static csci5408.catme.authentication.AuthConfig.AUTH_COOKIE_NAME;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,95 +13,86 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import static csci5408.catme.authentication.AuthConfig.AUTH_COOKIE_NAME;
+import csci5408.catme.authentication.ISessionStore;
+import csci5408.catme.dao.UserDao;
+import csci5408.catme.domain.User;
+import csci5408.catme.dto.UserSummary;
+import csci5408.catme.service.AuthenticationService;
+import csci5408.catme.service.UserService;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
-    final
-    AuthenticationManager authenticationManager;
+	final AuthenticationManager authenticationManager;
 
-    final
-    ISessionStore ISessionStore;
+	final ISessionStore ISessionStore;
 
-    final
-    UserService userService;
+	final UserService userService;
 
-    final
-    BCryptPasswordEncoder bCryptPasswordEncoder;
+	final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    final
-    UserDao userDao;
+	final UserDao userDao;
 
-    public AuthenticationServiceImpl(
-            AuthenticationManager authenticationManager,
-            ISessionStore ISessionStore, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, UserDao userDao) {
-        this.authenticationManager = authenticationManager;
-        this.ISessionStore = ISessionStore;
-        this.userService = userService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        this.userDao = userDao;
-    }
+	public AuthenticationServiceImpl(AuthenticationManager authenticationManager, ISessionStore ISessionStore,
+			UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, UserDao userDao) {
+		this.authenticationManager = authenticationManager;
+		this.ISessionStore = ISessionStore;
+		this.userService = userService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		this.userDao = userDao;
+	}
 
+	@Override
+	public boolean login(String email, String password, HttpServletResponse response) {
+		UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(email, password);
+		Authentication auth = authenticationManager.authenticate(authReq);
 
-    @Override
-    public boolean login(String email, String password, HttpServletResponse response) {
-        UsernamePasswordAuthenticationToken authReq
-                = new UsernamePasswordAuthenticationToken(email, password);
-        Authentication auth = authenticationManager.authenticate(authReq);
+		SecurityContext sc = SecurityContextHolder.getContext();
+		sc.setAuthentication(auth);
 
-        SecurityContext sc = SecurityContextHolder.getContext();
-        sc.setAuthentication(auth);
+		try {
+			if (this.isAuthenticated()) {
+				String cookieString = ISessionStore.setSession((UserSummary) auth.getPrincipal());
+				response.addCookie(new Cookie(AUTH_COOKIE_NAME, cookieString));
+			}
+		} catch (IllegalStateException e) {
+			System.out.println("Code probably called from CommandLineRunner");
+		}
 
-        try {
-            if(this.isAuthenticated()) {
-                String cookieString = ISessionStore.setSession((UserSummary)auth.getPrincipal());
-                response.addCookie(new Cookie(AUTH_COOKIE_NAME, cookieString));
-            }
-        } catch (IllegalStateException e) {
-            System.out.println("Code probably called from CommandLineRunner");
-        }
+		return sc.getAuthentication().isAuthenticated();
+	}
 
-        return sc.getAuthentication().isAuthenticated();
-    }
+	@Override
+	public UserSummary signUp(UserSummary user, String password) {
+		UserSummary existingUser = userService.getUserByEmailId(user.getEmailId());
+		if (existingUser != null) {
+			// User Exists
+			throw new RuntimeException("User Already Exists");
+		}
+		String encodedPassword = bCryptPasswordEncoder.encode(password);
+		return userService.createUser(user, encodedPassword);
+	}
 
-    @Override
-    public UserSummary signUp(UserSummary user, String password) {
-        UserSummary existingUser = userService.getUserByEmailId(user.getEmailId());
-        if(existingUser != null) {
-            // User Exists
-            throw new RuntimeException("User Already Exists");
-        }
-        String encodedPassword = bCryptPasswordEncoder.encode(password);
-        return userService.createUser(user, encodedPassword);
-    }
+	@Override
+	public boolean isAuthenticated() {
+		SecurityContext sc = SecurityContextHolder.getContext();
+		return sc.getAuthentication().isAuthenticated();
+	}
 
-    @Override
-    public boolean isAuthenticated() {
-        SecurityContext sc = SecurityContextHolder.getContext();
-        return sc.getAuthentication().isAuthenticated();
-    }
+	@Override
+	public void changePassword(UserSummary user, String password) {
+		String encodedPassword = bCryptPasswordEncoder.encode(password);
+		User u = userDao.findByEmail(user.getEmailId());
+		u.setPassword(encodedPassword);
+		userDao.update(u);
+	}
 
-    @Override
-    public void changePassword(UserSummary user, String password) {
-        String encodedPassword = bCryptPasswordEncoder.encode(password);
-        User u = userDao.findByEmail(user.getEmailId());
-        u.setPassword(encodedPassword);
-        userDao.update(u);
-    }
-    
-    public boolean isAdmin(String email, String password)
-    {
-    	User u = userDao.findByEmail(email);
-    	if(u.isAdmin()) {
-    	return true;
-    	}
-    	else {
-    		return false;
-    	}
-    }
+	public boolean isAdmin(String email, String password) {
+		User u = userDao.findByEmail(email);
+		if (u.isAdmin()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
