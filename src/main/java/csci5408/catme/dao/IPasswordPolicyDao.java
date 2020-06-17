19bd324c20d8a3d16fc24d18e02e7664a5d14ce5
Krupa Patel <kr635164@dal.ca>
2020-06-17 03:09:51 -0300

    Password Policy Dao and Dto created

--- src/main/java/csci5408/catme/dao/IPasswordPolicyDao.java
+++ src/main/java/csci5408/catme/dao/IPasswordPolicyDao.java
@@ -3,12 +3,14 @@
  */
 package csci5408.catme.dao;
 
+import csci5408.catme.dto.PasswordPolicy;
+
 /**
  * @author krupa
  *
  */
 public interface IPasswordPolicyDao {
 	
-	
+	PasswordPolicy find();
 
 }
---
+++ src/main/java/csci5408/catme/dao/impl/PasswordPolicyDaoImpl.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package csci5408.catme.dao.impl;
+
+import csci5408.catme.dao.IPasswordPolicyDao;
+import csci5408.catme.dto.PasswordPolicy;
+import csci5408.catme.service.IPasswordValidationService;
+
+/**
+ * @author krupa
+ *
+ */
+public class PasswordPolicyDaoImpl implements IPasswordPolicyDao {
+	
+	
+
+	@Override
+	public PasswordPolicy find() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
