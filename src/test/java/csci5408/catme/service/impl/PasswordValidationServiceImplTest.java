8c7e7ca84441de85ad048a8269684bbbfb44e919
Aman Vishnani <aman.vishnani@dal.ca>
2020-06-18 17:42:11 -0300

    Fix failing tests.

--- src/test/java/csci5408/catme/service/impl/PasswordValidationServiceImplTest.java
+++ src/test/java/csci5408/catme/service/impl/PasswordValidationServiceImplTest.java
@@ -1,15 +1,14 @@
 package csci5408.catme.service.impl;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
+import csci5408.catme.dao.impl.PasswordPolicyDaoImpl;
+import csci5408.catme.dto.PasswordPolicy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import csci5408.catme.dao.impl.PasswordPolicyDaoImpl;
-import csci5408.catme.dto.PasswordPolicy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class PasswordValidationServiceImplTest {
@@ -36,7 +35,7 @@ public class PasswordValidationServiceImplTest {
 		assertTrue(passwordValidationService.checkMinimum(count, minCount));
 		
 	}
-	
+
 	@Test
 	public void checkMinimumTest_False() {
 		int count = 5;
@@ -51,7 +50,6 @@ public class PasswordValidationServiceImplTest {
 		PasswordPolicy passwordPolicy = new PasswordPolicy() ;
 		passwordPolicy.setMinLength(3);
 		when(passwordPolicyDao.find()).thenReturn(passwordPolicy);
-		System.out.println("test ln"+passwordPolicy.getMinLength());
 		assertTrue(passwordValidationService.checkMinLength(password, passwordPolicy));
 	}
 	
@@ -62,7 +60,6 @@ public class PasswordValidationServiceImplTest {
 		PasswordPolicy passwordPolicy = new PasswordPolicy() ;
 		passwordPolicy.setMinLength(3);
 		when(passwordPolicyDao.find()).thenReturn(passwordPolicy);
-		System.out.println("test ln"+passwordPolicy.getMinLength());
 		assertFalse(passwordValidationService.checkMinLength(password, passwordPolicy));
 		
 	}
